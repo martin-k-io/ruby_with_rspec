0a447ceb7f93a039c1a20a15a55fd166c814a735
Martin K <martin.k.io.github@gmail.com>
2020-12-15 22:56:53 +0200

    Pass test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,10 @@
 class Card
-  attr_reader :type
+  attr_reader :rank
+  attr_reader :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 end
 
